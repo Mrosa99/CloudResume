5c65a04fd8e7336f08ec13dfaf509094146f4951
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2024-03-22 23:17:45 -0400

    updated skills format
    display flex added and text has been added

--- public/index.js
+++ public/index.js
@@ -35,3 +35,22 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementsByClassName("bar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
